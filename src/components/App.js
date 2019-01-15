e9497042cd50f9eaffc0b463c27d932cbcbedcfc
dcanivel-at-325274068097 <IESzg7L/tD0VMsuy/YKjuPgawtjynvmwD+rO24MtpGs=>
2019-01-15 09:26:07 -0500

    start routing

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,65 @@
 import React, { Component } from "react";
-import { withAuthenticator, AmplifyTheme } from "aws-amplify-react";
-import { AutoComplete } from "element-react";
+import { Authenticator, AmplifyTheme } from "aws-amplify-react";
+import Dashboard from "./Dashboard";
+import { Auth, Hub } from "aws-amplify";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 export class App extends Component {
-  render() {
-    return (
-      <div>
-        <h1>App</h1>
-      </div>
+  state = { user: null };
+
+  async componentDidMount() {
+    //Auth.signOut().then(c => console.log(c));
+    this.getUserData();
+    Hub.listen("auth", this, "onHubCapsule");
+  }
+
+  getUserData = async () => {
+    try {
+      const user = await Auth.currentAuthenticatedUser();
+      user ? this.setState({ user }) : this.setState({ user: null });
+      console.log(user);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  onHubCapsule = capsule => {
+    switch (capsule.payload.event) {
+      case "signIn":
+        console.log("signed in");
+        this.getUserData();
+        break;
+      case "signUp":
+        console.log("sign up");
+        break;
+      case "signOut":
+        console.log("sign out");
+        this.setState({ user: null });
+        break;
+      default:
+        // console.log(capsule.payload.event);
+        return;
+    }
+  };
+
+  renderHelper = () => {
+    const { user } = this.state;
+    return !user ? (
+      <Authenticator theme={theme} />
+    ) : (
+      <Router>
+        <>
+          {/* Routes */}
+          <div className="app-container">
+            <Route exact path="/" component={HomePage} />
+          </div>
+        </>
+      </Router>
     );
+  };
+
+  render() {
+    return <div>{this.renderHelper()}</div>;
   }
 }
 
@@ -39,4 +90,4 @@ const theme = {
   }
 };
 
-export default withAuthenticator(App, true, [], null, theme);
+export default App;
