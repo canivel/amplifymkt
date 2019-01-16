b3855b2cbed72ad441f5ff5748e7bc8d02492ccb
Danilo Canivel <d.canivel@gmail.com>
2019-01-16 15:02:46 -0500

    add new product start

--- src/components/NewProduct.js
+++ src/components/NewProduct.js
@@ -1,10 +1,68 @@
 import React, { Component } from "react";
+import { PhotoPicker } from "aws-amplify-react";
+import { Form, Input, Radio, Button } from "element-react";
 
 export class NewProduct extends Component {
+  state = {
+    desctiption: "",
+    price: "",
+    shipped: false
+  };
+
+  handleAddProduct = event => {
+    event.preventDefault();
+    console.log(event);
+  };
+
   render() {
+    const { shipped } = this.state;
     return (
-      <div>
-        <h1>New Product</h1>
+      <div className="flex-center">
+        <h2 className="header">Add New Product</h2>
+        <div>
+          <Form className="market-header">
+            <Form.Item lable="Add Product Descrition">
+              <Input
+                type="text"
+                icon="information"
+                placeholder="Description"
+                onChange={description => this.setState({ description })}
+              />
+            </Form.Item>
+            <Form.Item lable="Set Product Price">
+              <Input
+                type="number"
+                icon="plus"
+                placeholder="Price ($USD)"
+                onChange={price => this.setState({ price })}
+              />
+            </Form.Item>
+            <Form.Item lable="Is the product shipped or emailed to the customer">
+              <div className="text-center">
+                <Radio
+                  value="true"
+                  checked={shipped === true}
+                  onChange={() => this.setState({ shipped: true })}
+                >
+                  Shipped
+                </Radio>
+                <Radio
+                  value="true"
+                  checked={shipped === false}
+                  onChange={() => this.setState({ shipped: false })}
+                >
+                  Emailed
+                </Radio>
+              </div>
+            </Form.Item>
+            <PhotoPicker />
+            <Form.Item>
+              <Button type="primary" onClick={this.handleAddProduct}>
+                Add Product
+              </Button>
+            </Form.Item>
+          </Form>
+        </div>
       </div>
     );
   }
