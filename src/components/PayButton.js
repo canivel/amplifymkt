a186916a084047644544a12bcec4c7f2e2146e71
Danilo Canivel <d.canivel@gmail.com>
2019-01-17 15:52:53 -0500

    add api aws call

--- src/components/PayButton.js
+++ src/components/PayButton.js
@@ -1,13 +1,26 @@
 import React, { Component } from "react";
 import { Notification, Message } from "element-react";
 import StripeCheckout from "react-stripe-checkout";
+import { API } from "aws-amplify";
 
 const stripeConfing = {
   publishableAPIKey: "pk_test_rRsEsWJqdzJVsUsTri4cILuq",
   currency: "USD"
 };
 export class PayButton extends Component {
-  handleCharge = () => {};
+  handleCharge = async token => {
+    try {
+      const body = {
+        body: {
+          token
+        }
+      };
+      const result = await API.post("orderlambda", "/charge", body);
+      console.log(result);
+    } catch (err) {
+      console.error(err);
+    }
+  };
 
   render() {
     const { product, user } = this.props;
